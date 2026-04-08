7bb551f30795eee753cb32d8c419c30914c06996
Artem Yur <artemonodon@gmail.com>
2026-04-08 03:29:54 +0200

    'Solution'

--- services/movie_session.py
+++ services/movie_session.py
@@ -14,7 +14,7 @@ def create_movie_session(movie_show_time: datetime.datetime,
                                        cinema_hall_id=cinema_hall_id)
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
